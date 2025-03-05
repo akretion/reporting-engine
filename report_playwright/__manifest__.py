9b00c0fc571a2850621d939a363c32708f4cabe7
Francois Poizat <francois.poizat@gmail.com>
2025-03-05 16:45:17 +0100

    initial commit

---
+++ report_playwright/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2025 Akretion (http://akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Playwright Report Engine",
+    "summary": "Reporting engine based on playwright",
+    "version": "16.0.1.0.5",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "author": "François Poizat",
+    "website": "https://github.com/OCA/reporting-engine",
+    "depends": ["web"],
+    "external_dependencies": {
+        "python": ["playwright"],
+    },
+    "assets": {
+        "web.assets_backend": [
+            "report_playwright/static/src/js/playwright_action_service.esm.js",
+        ],
+    },
+    "data": [
+    ],
+    "installable": True,
+}
