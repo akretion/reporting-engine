0f7b0fb7e9601ecd46a94602df80a8e2d089bc7b
Francois Poizat <francois.poizat@gmail.com>
2025-03-06 12:01:53 +0100

    update authors with Akretion

--- report_playwright/__manifest__.py
+++ report_playwright/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "16.0.1.0.5",
     "category": "Reporting",
     "license": "AGPL-3",
-    "author": "François Poizat, Odoo Community Association (OCA)",
+    "author": "François Poizat (Akretion), Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/reporting-engine",
     "depends": ["web"],
     "external_dependencies": {
